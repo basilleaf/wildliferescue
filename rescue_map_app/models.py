ea8eab9059453c8a0540c1894178c86d2d112c5c
lisa ballard <lballard.cat@gmail.com>
2013-06-03 12:17:30 -0700

    now can fetch its data from scraperwiki

--- rescue_map_app/models.py
+++ rescue_map_app/models.py
@@ -6,7 +6,7 @@ class Facility(models.Model):
     name = models.CharField(max_length=60)
     phone = models.CharField(max_length=17, null=True, blank=True)
     specializations = models.CharField(max_length=200, null=True, blank=True)
-    scraperwiki_id = models.IntegerField()
+    scraperwiki_id = models.IntegerField(null=True, blank=True)
     address = models.CharField(max_length=100, null=True, blank=True)
     state = models.CharField(max_length=2, null=True, blank=True)
     zip_code = models.CharField(max_length=13, null=True, blank=True)
--- sync_feed.py
+++ sync_feed.py
@@ -1,15 +1,37 @@
+"""
+    check the scraperwiki source and import new additions
+    try to find addresses for any that don't have.
+    Runs occasionally on heroku scheduler
+
+    google api search for an address and update those fields too
+
+"""
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-    check the scraperwiki source and import any changes
-    try to find addresses for any that don't have.
-    Runs on heroku scheduler
-"""
+
+# setup for running as shell script
 from rescue_map_project import settings
 from django.core.management import setup_environ
 setup_environ(settings)
 
-from rescue_map_app.models import *
+# script imports
+import urllib2
+import json
+from django.db.utils import DatabaseError
+from rescue_map_app.models import Facility
 
-place = Facility(county="Alameda", city="Hayward", name="Homer Simpson", scraperwiki_id=3)
-place.save()
+url = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=california_wildlife_rehab_facilities&query=select+*+from+`swdata`'
+page = json.loads(urllib2.urlopen(url).read())
+
+for p in page:
+    # facility = Facility(county="Alameda", city="Hayward", name="Homer Simpson", scraperwiki_id=3)
+    facility = Facility(county=p["county"], city=p["city"], name=p["name"])
+
+    try:
+        facility.save()
+        print "added new " + ' '.join([p["county"], p["city"], p["name"]])
+    except DatabaseError:
+        print "we all good " + p["name"]
+        pass
+
+

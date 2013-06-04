95f54865bb05f89c42ce4e235f4bc942dacf72c2
lisa ballard <lballard.cat@gmail.com>
2013-06-03 21:40:00 -0700

    sync_feed is refactored to be more classy

--- sync_feed.py
+++ sync_feed.py
@@ -1,79 +1,122 @@
-"""
-    check the scraperwiki source and import new additions
-    try to find addresses for any that don't have.
-    Runs occasionally on heroku scheduler
-
-    google api search for an address and update those fields too
-
-"""
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+
+    TODO
+    this is just another scraperwiki, branch this! get rid of django and models
+
+    grabs data from remote json feed, inserts/updates local database
+    does address lookups on google maps api
+    --- --- ---
+    on scraperwiki there is a json feed from a scraped website
+    website is a listing of all California Wildlife Rehab Facilities
+    site lists city and state but no address.
+
+    this script takes the data from that feed and puts it into local database
+    then tries to do googlemaps address lookups on each
+
+"""
+# scraperwiki url
+json_url = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=california_wildlife_rehab_facilities&query=select+*+from+`swdata`'
 
 # setup for running as shell script
-from rescue_map_project import settings
 from django.core.management import setup_environ
+from rescue_map_project import settings
 setup_environ(settings)
-
-try:
-    GOOG_API_KEY = settings.GOOG_API_KEY
-except AttributeError:
-    try:
-        from rescue_map_project.settings_local import *
-    except ImportError:
-        pass
-
 
 # script imports
 import time
 import urllib2
 import json
-from googlemaps import GoogleMaps
+from googlemaps import GoogleMaps, GoogleMapsError
 from django.db import connection
 from django.db.utils import IntegrityError
-
 from rescue_map_app.models import Facility
+from random import randrange
 
-gmaps = GoogleMaps(GOOG_API_KEY)
-
-def get_geo_loc(search_str):
-    try:
-        loc = gmaps.local_search(search_str)
-        return loc
-    except GoogleMapsError:  # try again
-        time.sleep(2)
-        get_geo_loc(search_str)
-
-# look for any new additions to our json data feed and put thems in the db
-url = 'https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=json&name=california_wildlife_rehab_facilities&query=select+*+from+`swdata`'
-try:
-    page = json.loads(urllib2.urlopen(url).read())
-except:
-    import sys
-    sys.exit("could not connect to scraperwiki")
+GOOG_API_KEY = settings.GOOG_API_KEY
 
 
-for p in page:
-    # facility = Facility(county="Alameda", city="Hayward", name="Homer Simpson", scraperwiki_id=3)
-    facility = Facility(county=p["county"], city=p["city"], name=p["name"], state="CA")
+class sync_feed:
 
-    try:
-        facility.save()
-        print "added new " + ' '.join([p["county"], p["city"], p["name"]])
-    except IntegrityError:
-        connection._rollback()
-        # print "we all good " + p["name"]
-        pass
+    tries = 0
+    max_tries = 150
 
-    fac = Facility.objects.get(city=p["city"], name=p["name"])
+    gmaps = GoogleMaps(GOOG_API_KEY)
 
-    if not facility.address:
-        loc = get_geo_loc('"' + str(fac.name) + '" ' + str(fac.city) + ' ' + 'California')
+    def get_data(self, json_url):
+        """
+            fetch json data from fussy remote url
+        """
         try:
-            fac.address = loc[u'responseData']['results'][0][u'streetAddress']
-            fac.save()  # address found, save to db
-            # print "saved address for " + fac.name + "\n " + fac.address + "\n" + fac.city
+            return json.loads(urllib2.urlopen(json_url).read())
+        except:
+            # couldn't connect to scraperwiki, chill for a few secs and try again
+            self.tries = self.tries + 1
+            if self.tries < self.max_tries:
+                time.sleep(randrange(10))  # wait time is random bt 1-10 seconds
+                self.get_data(json_url)
+            else:
+                import sys
+                sys.exit("max tries get_data")
+
+    def search_google_maps(self, search_str):
+        """
+            send a search string to google maps interface
+            returns response
+        """
+        try:
+            loc = self.gmaps.local_search(search_str)
+            return loc
+        except GoogleMapsError:
+        # googlemaps failure usually connection, chill a few secs and try again
+            self.tries = self.tries + 1
+            if self.tries < self.max_tries:
+                time.sleep(randrange(10))
+                self.search_google_maps(search_str)
+            else:
+                import sys
+                sys.exit("max tries search_google_maps")
+
+    def find_facility(self, json_place):
+        """
+            given a place from the data source in json, add or find our model object, returns model obj
+        """
+        # place = Facility(county="Alameda", city="Hayward", name="Homer Simpson", scraperwiki_id=3)  # this works
+        place = Facility(county=json_place["county"], city=json_place["city"], name=json_place["name"], state="CA")
+
+        try:
+            place.save()
+            print "added new " + ' '.join([json_place["county"], json_place["city"], json_place["name"]])
+        except IntegrityError:
+            connection._rollback()  # oh hai pgsql
+            # print "we all good " + json_place["name"]
+            pass
+
+        return Facility.objects.get(city=json_place["city"], name=json_place["name"])
+
+    def update_address(self, place):
+        """
+            using the name and city lookup the address in googlemaps
+            and update the address in model
+        """
+        loc = self.search_google_maps('"' + str(place.name) + '" ' + str(place.city) + ' ' + 'California')
+        place.address = ''
+        try:
+            place.address = loc[u'responseData']['results'][0][u'streetAddress']
+            print "saved new address for " + place.name + "\n " + place.address + "\n" + place.city
         except IndexError:
-            print "no address for " + fac.name + " in " + fac.city
+            print "no address for " + place.name + " in " + place.city
+        except TypeError:
+            print "no address for " + place.name + " in " + place.city
+        place.save()  # address found, save to db
 
 
+if __name__ == "__main__":
 
+    sf = sync_feed()
+    data = sf.get_data(json_url)
+
+    for j in data:
+        place = sf.find_facility(j)
+        # sf.update_address(place)
